c4e3e40ddd910aa00334c18e014e0ad454972616
Markkus Hipponen <markkus.hipponen@khk.ee>
2020-09-16 10:53:10 +0300

    created and tested product description

---
+++ product.py
@@ -0,0 +1,32 @@
+class Product:
+
+    def __init__(self, name, price, amount):
+        self.setName(name)
+        self.setPrice(price)
+        self.setAmount(amount)
+
+    # product test view
+
+    def __repr__(self):
+        return "Product: {} \nprice: {}\namount: {}".format(self.getName(), self.getPrice(), self.getAmount())
+
+    # setters
+    def setName(self, name):
+        self.__name = name
+
+    def setPrice(self, price):
+        self.__price = price
+
+    def setAmount(self, amount):
+        self.__amount = amount
+
+
+    # getters
+    def getName(self):
+        return self.__name
+
+    def getPrice(self):
+        return self.__price
+
+    def getAmount(self):
+        return self.__amount
---
+++ ui.py
@@ -0,0 +1,13 @@
+# import classes and files
+
+from product import Product
+
+# create products
+bread = Product("bread", 0.80, 10)
+milk = Product("milk", 0.50, 50)
+wine = Product("wine", 5.60, 5)
+
+# test product view
+print(bread)
+print(milk)
+print(wine)
